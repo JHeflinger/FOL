87a9a591c06ed1dc5db8b569ec8c1fda0849c190
Jason Heflinger <jasonheflinger@gmail.com>
2023-10-26 18:33:11 -0500

    added play,pause,reset and custom config

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -9,35 +9,80 @@ namespace Game
 		public int Size;
 		public float Frequency;
 		private float Count;
-		private List<List<Cell>> Cells;
+		private bool Playing = false;
+		private List<List<Entity>> Cells;
+		private List<List<int>> Neighbors;
+		private List<List<bool>> Status;
+		private LocalInput input;
+
+		public void Play()
+        {
+			Playing = true;
+        }
+
+		public void Pause()
+        {
+			Playing = false;
+        }
+
+		public void Reset()
+        {
+			ResetBoard();
+        }
+
+		void ResetBoard()
+		{
+			for (int r = 0; r < Size; r++)
+			{
+				for (int c = 0; c < Size; c++)
+				{
+					Cells[r][c].GetComponent<SpriteRendererComponent>().Color = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+					Neighbors[r][c] = 0;
+					Status[r][c] = false;
+				}
+			}
+		}
 
 		void CreateBoard()
 		{
-			Cells = new List<List<Cell>>();
+			Cells = new List<List<Entity>>();
+			Neighbors = new List<List<int>>();
+			Status = new List<List<bool>>();
 			for (int r = 0; r < Size; r++)
             {
-				List<Cell> row = new List<Cell>();
+				List<Entity> row = new List<Entity>();
+				List<int> nrow = new List<int>();
+				List<bool> srow = new List<bool>();
 				for (int c = 0; c < Size; c++)
                 {
-					Cell cell = Scene.CreateEntity<Cell>("Cell");
+					Entity cell = Scene.CreateEntity("Cell");
 					cell.Translation = new Vector3((float)r - ((float)Size/2), (float)c - ((float)Size /2), 0.0f);
-					cell.Scale = new Vector3(0.9f, 0.9f, 1.0f);
+					cell.Scale = new Vector3(0.9f, 0.9f, 1.0f); 
+					cell.AddComponent<SpriteRendererComponent>();
+					cell.GetComponent<SpriteRendererComponent>().Color = new Vector4(1);
+					cell.GetComponent<SpriteRendererComponent>().Color = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
 					row.Add(cell);
+					nrow.Add(0);
+					srow.Add(false);
                 }
 				Cells.Add(row);
+				Neighbors.Add(nrow);
+				Status.Add(srow);
             }
 		}
 		
 		void LoadConfig(List<List<int>> arr)
         {
 			for (int i = 0; i < arr.Count; i++)
-            {
-				Cells[arr[i][0]][arr[i][1]].Revive();
-            }
+			{
+				Cells[arr[i][0]][arr[i][1]].GetComponent<SpriteRendererComponent>().Color = new Vector4(1.0f);
+				Status[arr[i][0]][arr[i][1]] = true;
+			}
         }
 
 		void OnCreate()
 		{
+			input = new LocalInput();
 			CreateBoard();
 			List<List<int>> config = new List<List<int>>()
 			{
@@ -60,29 +105,86 @@ namespace Game
 
 		void OnUpdate(float ts)
 		{
-			Count += ts;
-			if (Count > Frequency)
+			GridControl();
+			if (Playing)
 			{
-				Count -= Frequency;
-				for (int r = 0; r < Size; r++)
+				Count += ts;
+				if (Count > Frequency)
 				{
-					for (int c = 0; c < Size; c++)
+					Count -= Frequency;
+					for (int r = 0; r < Size; r++)
 					{
-						Cells[r][c].Ready = true;
-						if (Cells[r][c].Alive)
+						for (int c = 0; c < Size; c++)
 						{
-							for (int i = 0; i < 3; i++)
+							if (Status[r][c])
 							{
-								for (int j = 0; j < 3; j++)
+								for (int i = 0; i < 3; i++)
 								{
-									if ((i != 1 || j != 1) && ValidCoordinate(r - 1 + i, c - 1 + j))
+									for (int j = 0; j < 3; j++)
 									{
-										Cells[r - 1 + i][c - 1 + j].Neighbors += 1;
+										if ((i != 1 || j != 1) && ValidCoordinate(r - 1 + i, c - 1 + j))
+										{
+											Neighbors[r - 1 + i][c - 1 + j] += 1;
+										}
 									}
 								}
 							}
 						}
 					}
+					ProcessGeneration();
+				}
+			}
+		}
+
+		void GridControl()
+		{
+			if (input.IsMouseButtonReleased(MouseCode.ButtonLeft))
+			{
+				long id = Visual.GetHoveredEntityID();
+				for (int r = 0; r < Size; r++)
+				{
+					for (int c = 0; c < Size; c++)
+					{
+						if (id == Cells[r][c].ID)
+                        {
+							if (Status[r][c])
+                            {
+								Cells[r][c].GetComponent<SpriteRendererComponent>().Color = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+								Status[r][c] = false;
+                            } else
+							{
+								Cells[r][c].GetComponent<SpriteRendererComponent>().Color = new Vector4(1.0f);
+								Status[r][c] = true;
+							}
+                        }
+					}
+				}
+			}
+		}
+
+		void ProcessGeneration()
+		{
+			for (int r = 0; r < Size; r++)
+			{
+				for (int c = 0; c < Size; c++)
+				{
+					if (Status[r][c])
+					{
+						if (Neighbors[r][c] != 2 && Neighbors[r][c] != 3)
+						{
+							Cells[r][c].GetComponent<SpriteRendererComponent>().Color = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+							Status[r][c] = false;
+						}
+					}
+					else
+					{
+						if (Neighbors[r][c] == 3)
+						{
+							Cells[r][c].GetComponent<SpriteRendererComponent>().Color = new Vector4(1.0f);
+							Status[r][c] = true;
+						}
+					}
+					Neighbors[r][c] = 0;
 				}
 			}
 		}
---
+++ Assets/Scripts/ResetButton.cs
@@ -0,0 +1,27 @@
+using System;
+using Flora;
+
+namespace Game
+{
+	public class ResetButton : Entity
+	{
+		LocalInput input;
+
+		void OnCreate()
+		{
+			input = new LocalInput();
+		}
+
+		void OnDestroy()
+		{
+		
+		}
+
+		void OnUpdate(float ts)
+		{
+			if (input.IsMouseButtonReleased(MouseCode.ButtonLeft))
+				if (Visual.GetHoveredEntityID() == ID)
+					Scene.FindEntityByName("Game Controller").As<Controller>().Reset();
+		}
+	}
+}
